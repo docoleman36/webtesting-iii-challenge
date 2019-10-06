f019c0a7567f400846c390aa77547d7436b08027
Donald Coleman <donaldcoleman@Donalds-MacBook-Pro.local>
2019-10-06 16:35:38 -0700

    Completed project

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -18,8 +18,66 @@ test('contains buttons', () => {
 test('on click changes button text', () => {
   const locked = false;
   const closed = false;
-  const toggleClose = jest.fn();
+  const toggleClosed = jest.fn();
 
-  const { getByText } = render(<Controls locked={locked} closed={closed} toggleClosed={toggleClose} />);
+  const { getByText, findByText } = render(
+  <Controls locked={locked} closed={closed} toggleClosed={toggleClosed} />);
 
+    const closeBtn = getByText(/close gate/i);
+
+    fireEvent.click(closeBtn);
+
+    const openBtn = findByText(/open gate/i);
+
+    expect(openBtn).toBeDefined();
+    expect(toggleClosed).toHaveBeenCalled();
+});
+
+test("lock button changes to reflext state the door will be in if closed", () => {
+  const locked = false; 
+  const closed = true;
+  const toggleLocked = jest.fn();
+  const { getByText, findByText } = render(
+    <Controls locked={locked} closed={closed} toggleLocked={toggleLocked} />
+  )
+
+  const lockBtn = getByText(/lock gate/i);
+
+  fireEvent.click(lockBtn);
+
+  const unlockBtn = findByText(/unlock gate/i);
+
+  expect(unlockBtn).toBeDefined();
+  expect(toggleLocked).toHaveBeenCalled();
 })
+
+test("the closed toggle button is disabled if gate is locked", () => {
+  const locked = true; 
+  const closed = true;
+  const toggleClosed = jest.fn();
+
+  const { getByText } = render(
+    <Controls locked={locked} closed={closed} toggleClosed={toggleClosed}/>
+  );
+
+  const openBtn = getByText(/open gate/i);
+
+  fireEvent.click(openBtn);
+
+  expect(toggleClosed).not.toHaveBeenCalled();
+})
+
+test("the locked toggle button is disabled if gate is open", () => {
+  const locked = false;
+  const closed = false;
+  const toggleLocked = jest.fn();
+  const { getByText } = render(
+    <Controls locked={locked} closed={closed} toggleLocked={toggleLocked} />
+  );
+
+  const lockBtn = getByText(/lock gate/i);
+
+  fireEvent.click(lockBtn);
+
+  expect(toggleLocked).not.toHaveBeenCalled();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,7 +1,86 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Display from './Display';
+import React from "react";
+import Display from "./Display.js";
+import { render } from "@testing-library/react";
 
-test('Display renders without crashing', () => {
+//tests if component runs
+test("Component renders", () => {
   render(<Display />);
-})
+});
+
+test("displays if gate is open/closed and if it is locked/unlocked", () => {
+  //destructures getByText to check dashboard by default
+  const { getByText } = render(<Display />);
+
+  //finds the divs by text to see if they are present
+  const open = getByText(/open/i);
+  const locked = getByText(/unlocked/i);
+
+  //asserts that both elements are present
+  expect(open).toBeDefined();
+  expect(locked).toBeDefined();
+});
+
+test("displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise", () => {
+    //create a variable to hold boolean value
+  var toggled = true;
+
+  //create a function to change boolean value
+  const toggler = () => {
+    toggled = false;
+  };
+
+  //destructures getByText and findByText to check dashboard by default
+  const { getByText, findByText } = render(<Display closed={toggled} />);
+
+  //finds element by text of closed
+  const closed = getByText(/closed/i);
+
+  //assert that it is present
+  expect(closed).toBeDefined();
+
+  //run function to change boolean value
+  toggler();
+
+  //finds element by text
+  const open = findByText(/open/i);
+
+  //asserts that the text has changed
+  expect(open).toBeDefined();
+});
+
+//almost identical to previous test, check comments there
+test("displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise", () => {
+  var toggled = true;
+  const toggler = () => {
+    toggled = false;
+  };
+  const { getByText, findByText } = render(<Display locked={toggled} />);
+
+  const locked = getByText(/locked/i);
+
+  expect(locked).toBeDefined();
+
+  toggler();
+
+  const unlocked = findByText(/unlocked/i);
+
+  expect(unlocked).toBeDefined();
+});
+
+test("when `locked` or `closed` use the `red-led` class", () => {
+    //allows me to check the image of display with values of props passed
+  const { container } = render(<Display locked={true} closed={true} />);
+
+  //creates a nodelist of items with the class of .red-led
+  const redLeds = container.querySelectorAll(".red-led");
+
+  //length of nodelist should be 2
+  expect(redLeds.length).toBe(2);
+});
+
+//almost identical to previous test, check that
+test("when `unlocked` or `open` use the `green-led` class", () => {
+  const { container } = render(<Display locked={false} closed={false} />);
+  const greenLeds = container.querySelectorAll(".green-led");
+  expect(greenLeds.length).toBe(2);
+});
